a2c581a5133e866b46f86d4664ad8ae209970e1a
binoddshah <binoddshah@gmail.com>
2020-10-18 21:50:41 +0545

    Saving to github

---
+++ OnlineDigitization/OnlineDigitization/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineDigitization.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
